995e54ec308acfab58e6ae2bff2fdf492509f55c
i-anandsingh <singh.anand974@gmail.com>
2023-12-31 12:30:25 +0530

    Project Setup for Movie Application

---
+++ src/main/java/com/movie/details/MovieTimestampApplication.java
@@ -0,0 +1,13 @@
+package com.movie.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTimestampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTimestampApplication.class, args);
+	}
+
+}
